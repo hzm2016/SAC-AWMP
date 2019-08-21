d4fab9f2aec6b7e3371bc062ff46684dd8b51367
kuangen <zhangkuangen@qq.com>
2019-08-20 23:12:05 -0700

    add zmp control to adjust the impedance parameters, but the result is worse.

--- leg/controllers/ZMP_control/utils.py
+++ leg/controllers/ZMP_control/utils.py
@@ -3,16 +3,17 @@ from controller import Robot
 import math
 import time
 import random
+import numpy as np
 
 class Robot_timer(QThread, QObject):
-    sensor_signal = pyqtSignal(float, float, float)
+    sensor_signal = pyqtSignal(float, float, float, float)
     def __init__(self, robot, ankle_z_motor, ankle_z_position, time_step):
         super(Robot_timer, self).__init__()
         self.robot = robot
         self.ankle_z_motor = ankle_z_motor
         self.ankle_z_position = ankle_z_position
         self.time_step = time_step
-        self.k = 5.00
+        self.k = 10.00
         self.b = self.k / 2.0
         self.angle_e = 0.0
         self.is_run = True
@@ -25,31 +26,44 @@ class Robot_timer(QThread, QObject):
 
     def run(self):
         self.is_run = True
-        angle_z_list = [0, 0]
-        time_list = [0, 0]
+        angle_vec = np.asarray([0.0, 0.0, 0.0])
+        x_zmp_vec = np.asarray([0.0, 0.0, 0.0])
+        angle_v_vec = np.asarray([0.0, 0.0])
+        time_vec = np.asarray([0.0, 0.0])
         g = 9.81
-        tau_max = 1.0
-        tau_change_time = 0.1
         # self.robot.step(self.time_step) is a timer
         while self.robot.step(self.time_step) != -1 and self.is_run:
-            angle_z_list = fifo_list(angle_z_list, self.ankle_z_position.getValue())
-            time_list = fifo_list(time_list, self.robot.getTime())
-            if (time_list[-1] % 10.0) < 0.5:
+            angle_vec = fifo_list(angle_vec, self.ankle_z_position.getValue())
+            angle_v = (angle_vec[-1] - angle_vec[-2]) / (self.time_step * 1e-3)
+            angle_v_vec = fifo_list(angle_v_vec, angle_v)
+            angle_a = (angle_v_vec[-1] - angle_v_vec[-2]) / (self.time_step * 1e-3)
+            time_vec = fifo_list(time_vec, self.robot.getTime())
+            if time_vec[-1] < 1.0:
                 # print('robot time: %s, real time: %s' % (time_list[-1], time.time() - start))
-                tau_init = calc_init_tau((time_list[-1] % 10.0) , tau_max, tau_change_time)
-                self.ankle_z_motor.setTorque(tau_init)
+                self.ankle_z_motor.setPosition(-0.1)
                 continue
-            # if 0 == time_list[1] - time_list[0]:
-            #     continue
-            angle_z_v = (angle_z_list[1] - angle_z_list[0]) / (self.time_step * 1e-3)
-            # angle_z_v = (theta_list[1] - theta_list[0]) * self.time_step
-            tau = self.k * (self.angle_e - angle_z_list[-1]) - self.b * angle_z_v - \
-                  5.0 * g * 0.4 * math.sin(angle_z_list[-1])
-            self.ankle_z_motor.setTorque(tau)
-            self.sensor_signal.emit(angle_z_list[1], angle_z_v, tau)
-            # print('Emit sensor_signal: ', theta_list[1], angle_z_v, self.ankle_z_motor.getTorqueFeedback())
-            tau_max = random.uniform(-2.0, 2.0)
-            tau_change_time = random.uniform(0, 0.4)
+            tau = self.k * (self.angle_e - angle_vec[-1]) - self.b * angle_v - \
+                  5.0 * g * 0.4 * math.sin(angle_vec[-1])
+            if random.uniform(0, 1.0) > 0.8:
+                tau_noise = random.uniform(2, 5.0) + 5 * math.sin(3.0 * time_vec[-1])
+            else:
+                tau_noise = 0.0
+            self.ankle_z_motor.setTorque(tau + tau_noise)
+            x_zmp_vec = fifo_list(x_zmp_vec, calc_x_zmp_by_mc(angle_vec[1], angle_v, angle_a))
+            # x_zmp = x_zmp_vec[np.argmin(np.abs(x_zmp_vec))]
+            x_zmp = x_zmp_vec[-1]
+            x_zmp_th = 0.03
+            # if abs(x_zmp) < x_zmp_th:
+            #     self.k = 10.0
+            #     self.b = self.k / 2.0
+            # elif (abs(x_zmp) < x_zmp_th * 1.5) and abs(x_zmp_vec[-1]) < abs(x_zmp_vec[-2]):
+            # # if (abs(x_zmp) > x_zmp_th / 2.0) and (abs(x_zmp) < x_zmp_th * 1.5) and \
+            # #         abs(x_zmp_vec[-1]) < abs(x_zmp_vec[-2]):
+            #     self.k *= x_zmp_th / abs(x_zmp)
+            #     self.b = self.k / 2.0
+            #     print('x_zmp: %s, k: %s, b: %s.' % (x_zmp, self.k, self.b))
+            self.sensor_signal.emit(angle_vec[1], angle_v, angle_a, x_zmp)
+
     def stop(self):
         self.is_run = False
 
@@ -85,6 +99,8 @@ class Controller_timer(QThread, QObject):
 
         self.angle = 0.0
         self.angle_v = 0.0
+        self.angle_a = 0.0
+        self.x_zmp = 0.0
         self.tau = 0.0
         self.k = 5.00
         self.b = self.k / 2.0
@@ -112,6 +128,11 @@ class Controller_timer(QThread, QObject):
             # if 0 == time_list[1] - time_list[0]:
             #     time.sleep(1.0 / self.sampling_rate)
             #     continue
+            # F_x = self.ankle_x_linear_motor.getForceFeedback()
+            # F_y = self.ankle_y_linear_motor.getForceFeedback()
+            # print('x_zmp: %s, F_x: %s, Fy: %s' % (self.x_zmp, F_x, F_y))
+            # x_zmp = calc_x_zmp_by_mc(self.angle, self.angle_v, self.angle_a)
+            # print('x_zmp: ', x_zmp)
             # self.angle_e = angle_e_list[int(time_list[-1] / 5) % 2]
             # F_x = self.ankle_x_linear_motor.getForceFeedback()
             # F_y = self.ankle_y_linear_motor.getForceFeedback()
@@ -126,13 +147,6 @@ class Controller_timer(QThread, QObject):
             # self.k_a = calc_k_a(tau_design, self.k, self.b, self.angle_e, self.angle, self.angle_v)
             # self.update_impedance()
             time.sleep(1.0 / self.sampling_rate)
-
-        # for i in range(10):
-        #     if not self.is_run:
-        #         break
-        #     self.angle_e = angle_e_list[i % 2]
-        #     self.update_impedance()
-        #     time.sleep(5)
         self.robot_thread.stop()
 
     def update_impedance(self):
@@ -140,10 +154,11 @@ class Controller_timer(QThread, QObject):
         self.b *= self.k_a
         self.new_impedance.emit(self.k, self.b, self.angle_e)
 
-    def update_sensor_signal(self, angle, angle_v, tau):
+    def update_sensor_signal(self, angle, angle_v, angle_a, x_zmp):
         self.angle = angle
         self.angle_v = angle_v
-        self.tau = tau
+        self.angle_a = angle_a
+        self.x_zmp = x_zmp
         # print('Received sensor_signal: ', self.angle, self.angle_v, self.tau)
 
     def stop(self):
@@ -161,6 +176,13 @@ def calc_x_zmp(F_x, F_y, tau_m, m_f = 0.5, x_g = 0.025, h_ankle = 0.05):
     x_zmp = (F_x * h_ankle + m_f * 9.81 * x_g + tau_m) / (m_f * 9.81 + F_y)
     return x_zmp
 
+
+def calc_x_zmp_by_mc(angle, angle_v, angle_a, g=9.81, r = 0.4):
+    g_offset = g - (-r * angle_v**2 * math.cos(angle) + r*angle_a*math.sin(angle))
+    if g_offset < 1e-3:
+        return 0.0
+    else:
+        return -r * math.sin(angle) + (r * angle_v**2 * math.sin(angle) + r*angle_a*math.cos(angle)) / g_offset
 
 def calc_tau_m(F_x, F_y, x_zmp, m_f = 0.5, x_g = 0.025, h_ankle = 0.05):
     return (m_f * 9.81 + F_y) * x_zmp - F_x * h_ankle - m_f * 9.81 * x_g
