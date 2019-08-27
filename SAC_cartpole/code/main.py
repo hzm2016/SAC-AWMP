4f60a268539e2bf287e64bf76c2d6265d8b65e9f
kuangen <zhangkuangen@qq.com>
2019-08-26 22:33:44 -0700

    add model_based choice

--- SAC_cartpole/code/main.py
+++ SAC_cartpole/code/main.py
@@ -16,7 +16,7 @@ parser.add_argument('--env-name', default="Pendulum-v0",
                     help='name of the environment to run')
 parser.add_argument('--policy', default="Gaussian",
                     help='algorithm to use: Gaussian | Deterministic')
-parser.add_argument('--eval_only', type=bool, default=True,
+parser.add_argument('--eval_only', type=bool, default=False,
                     help='Only evaluates a policy without training (default:True)')
 parser.add_argument('--eval', type=bool, default=True,
                     help='Evaluates a policy a policy every 10 episode (default:True)')
@@ -26,23 +26,24 @@ parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor for reward (default: 0.99)')
 parser.add_argument('--tau', type=float, default=0.005, metavar='G',
                     help='target smoothing coefficient(τ) (default: 0.005)')
-parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+parser.add_argument('--lr', type=float, default=0.0002, metavar='G',
                     help='learning rate (default: 0.0003)')
-parser.add_argument('--alpha', type=float, default=0.05, metavar='G',
-                    help='Temperature parameter α determines the relative importance of the entropy term against the reward (default: 0.2)')
+parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
+                    help='Temperature parameter α determines the relative importance of the entropy term against the '
+                         'reward (default: 0.2)')
 parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
                     help='Temperature parameter α automaically adjusted.')
 parser.add_argument('--seed', type=int, default=456, metavar='N',
                     help='random seed (default: 456)')
 parser.add_argument('--batch_size', type=int, default=256, metavar='N',
                     help='batch size (default: 256)')
-parser.add_argument('--num_steps', type=int, default=10000, metavar='N',
+parser.add_argument('--num_steps', type=int, default=20000, metavar='N',
                     help='maximum number of steps (default: 1000000)')
 parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                     help='hidden size (default: 256)')
 parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
                     help='model updates per simulator step (default: 1)')
-parser.add_argument('--start_steps', type=int, default=100, metavar='N',
+parser.add_argument('--start_steps', type=int, default=1000, metavar='N',
                     help='Steps sampling random actions (default: 10000)')
 parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
                     help='Value target update per no. of updates per step (default: 1)')
--- SAC_cartpole/code/utils.py
+++ SAC_cartpole/code/utils.py
@@ -2,6 +2,7 @@ import math
 import torch
 import numpy as np
 
+
 def create_log_gaussian(mean, log_std, t):
     quadratic = -((0.5 * (t - mean) / (log_std.exp())).pow(2))
     l = mean.shape
@@ -9,6 +10,7 @@ def create_log_gaussian(mean, log_std, t):
     z = l[-1] * math.log(2 * math.pi)
     log_p = quadratic.sum(dim=-1) - log_z.sum(dim=-1) - 0.5 * z
     return log_p
+
 
 def logsumexp(inputs, dim=None, keepdim=False):
     if dim is None:
@@ -20,9 +22,11 @@ def logsumexp(inputs, dim=None, keepdim=False):
         outputs = outputs.squeeze(dim)
     return outputs
 
+
 def soft_update(target, source, tau):
     for target_param, param in zip(target.parameters(), source.parameters()):
         target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
 
 def hard_update(target, source):
     for target_param, param in zip(target.parameters(), source.parameters()):
@@ -37,27 +41,14 @@ def random_action(action_space):
 def calc_torque(state, k = 5.0, b = 0.5, k_g = 10.0):
     # state: cos(q), sin(q), dq
     theta = np.arctan2(state[1], state[0])
-    # torque = np.asarray([k * (0 - theta) - b * state[-1]]) - k_g * state[1]
     l_m = 1.0
     theta_0 = np.arcsin(4.0 / k_g)
     energy_error = 0.5 * k_g * (np.cos(theta_0) - np.cos(theta)) - \
                    k_g / (9.81 * l_m) * l_m ** 2.0 * state[-1] ** 2.0 / 3.0
     if energy_error >= 0:
-    # if abs(theta) >= abs(theta_0):
-    #     print(energy_error)
         torque = np.abs([k * (0 - theta) - b * state[-1]]) * np.sign(state[-1])
-        # torque = np.asarray([0.1 * energy_error * np.sign(state[-1])])
-        # theta = theta % 2*np.pi
-        # if state[-1] > 0:
-        #     theta -= 2*np.pi
-        # k_g = 0.0
-        # torque = np.asarray([k * state[-1]])
-        # torque = np.asarray([k * (0 - theta) - b * state[-1]])
-        # action = np.asarray([k * (0 - theta) - b * state[-1]]) - k_g * state[1]
-    # action = np.asarray([k * (0 - theta) - b * state[-1]])
     else:
         torque = np.asarray([k * (0 - theta) - b * state[-1]]) - k_g * state[1]
-    # torque = np.asarray([k * (0 - theta) - b * state[-1]]) - k_g * state[1]
     return torque, theta
 
 
@@ -70,7 +61,7 @@ def render_env(env, agent, k = None, b = None, k_g = None, model_based = False):
         if k is None or b is None or k_g is None:
             action = agent.select_action(state, eval=True)
             if model_based:
-                torque, theta = calc_torque(state, k=action[0], b=action[1], k_g=20.0)
+                torque, theta = calc_torque(state, k=action[0], b=action[1], k_g=10.0)
             else:
                 torque = action
             # print(action)
