c139989fdc2ee8ba33561f48aaa63e7c98cc305d
“KuangenZhang” <kuangen.zhang@alumni.ubc.ca>
2019-11-05 20:23:49 -0800

    update the maximum timesteps

--- code/main.py
+++ code/main.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("--start_timesteps", default=1e4,
                         type=int)  # How many time steps purely random policy is run for
     parser.add_argument("--eval_freq", default=5e3, type=int)  # How often (time steps) we evaluate
-    parser.add_argument("--max_timesteps", default=3e5, type=int)  # Max time steps to run environment for
+    parser.add_argument("--max_timesteps", default=1e6, type=int)  # Max time steps to run environment for
 
     parser.add_argument("--expl_noise", default=0.1, type=float)  # Std of Gaussian exploration noise
     parser.add_argument("--state_noise", default=0, type=float)  # Std of Gaussian exploration noise
